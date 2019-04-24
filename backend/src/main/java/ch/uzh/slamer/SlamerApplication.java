96c9fac8b2b3ca323e2a252090d9d99d9651183a
TPccHorizon <carlos.schweizer@hotmail.com>
2019-04-24 17:58:00 +0200

    added backend module

---
+++ backend/src/main/java/ch/uzh/slamer/SlamerApplication.java
@@ -0,0 +1,13 @@
+package ch.uzh.slamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlamerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlamerApplication.class, args);
+    }
+
+}
