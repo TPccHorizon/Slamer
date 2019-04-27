405b3409765e5155502a89cc846bb6694fe8bb7c
TPccHorizon <carlos.schweizer@hotmail.com>
2019-04-27 18:59:43 +0200

    removed codegen

--- backend/src/main/java/ch/uzh/slamer/service/WelcomeService.java
+++ backend/src/main/java/ch/uzh/slamer/service/WelcomeService.java
@@ -1,10 +1,46 @@
 package ch.uzh.slamer.service;
 
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @Component
 public class WelcomeService {
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String userName;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
     public String getWelcomeMessage() {
-        return "A message s";
+
+
+        String message;
+
+        try (Connection conn = DriverManager.getConnection(url, userName, password)){
+            System.out.println("Connected");
+            DSLContext create = DSL.using(conn, SQLDialect.POSTGRES);
+            Record result = create.select().from("SLA_USER").fetchOne();
+
+            Integer id = (Integer) result.getValue("id");
+            String firstName = (String) result.getValue("username");
+
+            message = "ID: " + id + " first name: " + firstName;
+        } catch (Exception e) {
+            message = "Failed";
+            e.printStackTrace();
+        }
+
+        return message;
     }
 }
