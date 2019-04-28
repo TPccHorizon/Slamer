a55bfed1f1d2a715322eaa374d2f5cc86e4dda3f
TPccHorizon <carlos.schweizer@hotmail.com>
2019-04-28 15:13:08 +0200

    basic frontend folder structure

--- backend/src/main/java/ch/uzh/slamer/controller/WelcomeController.java
+++ backend/src/main/java/ch/uzh/slamer/controller/WelcomeController.java
@@ -1,11 +1,13 @@
 package ch.uzh.slamer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.uzh.slamer.service.WelcomeService;
 
+@CrossOrigin(allowCredentials = "true")
 @RestController
 public class WelcomeController {
 
