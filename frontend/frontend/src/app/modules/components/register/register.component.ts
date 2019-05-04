3e67b748ed10ca90cc34267f5c523bd0d95a6e3e
TPccHorizon <carlos.schweizer@hotmail.com>
2019-05-04 15:42:36 +0200

    register component content

--- frontend/frontend/src/app/modules/components/register/register.component.ts
+++ frontend/frontend/src/app/modules/components/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../../core/services/authentication.service";
+import {SlaUserService} from "../../../core/services/sla-user.service";
+import {AlertService} from "../../../core/services/alert.service";
+import {first} from "rxjs/operators";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +12,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authenticationService: AuthenticationService,
+              private userService: SlaUserService,
+              private alertService: AlertService
+  ) {
+    if (this.authenticationService) {
+      this.router.navigate(['/']);
+    }
+  }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      partyName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(6)],
+    })
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(data => {
+        this.alertService.success('Registration successful', true);
+        this.router.navigate(['/login'])
+      }, error => {
+        this.alertService.error(error);
+        this.loading = false;
+      })
   }
 
 }
