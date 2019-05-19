b37b89e47009fe9a7f53149ac9af587929cb78e9
TPccHorizon <carlos.schweizer@hotmail.com>
2019-05-19 18:40:10 +0200

    fixed toolbar to stay sticky

--- frontend/frontend/src/app/core/navigation/navigation.component.ts
+++ frontend/frontend/src/app/core/navigation/navigation.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AuthenticationService} from "../services/authentication.service";
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+
+  @Input() themeColor = '';
 
   constructor(private authService: AuthenticationService) { }
 
