07e261fbd67edaf2f3b6a08eec3617bbf12d5d43
TPccHorizon <carlos.schweizer@hotmail.com>
2019-05-30 15:30:40 +0200

    added basic review component

---
+++ frontend/frontend/src/app/modules/sla-creation/sla-review/sla-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sla-review',
+  templateUrl: './sla-review.component.html',
+  styleUrls: ['./sla-review.component.css']
+})
+export class SlaReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
