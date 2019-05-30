b144b740af013768e677c7d72b01ed76581cfa96
TPccHorizon <carlos.schweizer@hotmail.com>
2019-05-30 18:33:54 +0200

    basic styling with mock data

--- frontend/frontend/src/app/modules/home/chart/chart.component.ts
+++ frontend/frontend/src/app/modules/home/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ChartType} from "chart.js";
 
 @Component({
   selector: 'app-chart',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
-  labels = ['Definition', 'Negotiation', 'Monitor', 'Termination'];
-  chartData = [2, 3, 20, 3];
-  chartType = 'doughnut';
+  labels = ['Definition', 'Negotiation', 'Monitoring', 'Management', 'Termination', 'Penalty Enforcement'];
+  chartData = [2, 3, 10, 3, 3, 2];
+  chartType: ChartType = 'doughnut';
 
   constructor() { }
 
