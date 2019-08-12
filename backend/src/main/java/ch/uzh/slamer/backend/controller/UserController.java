dedd2b3f5f0f281ff680d7ba4740a1d7233144b1
TPccHorizon <carlos.schweizer@hotmail.com>
2019-08-12 09:11:00 +0200

    api endpoint for monitoring services

--- backend/src/main/java/ch/uzh/slamer/backend/controller/UserController.java
+++ backend/src/main/java/ch/uzh/slamer/backend/controller/UserController.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @CrossOrigin(allowCredentials = "false", origins = "${security.allowed-origin}")
 @RestController
 public class UserController {
@@ -33,5 +36,13 @@ public class UserController {
             return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
         }
 
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "users/monitor")
+    public ResponseEntity<List<SlaUserDTO>> getMonitoringServices() {
+        List<SlaUser> monitoringServices = userRepository.getMonitoringServices();
+        List<SlaUserDTO> serviceDtos = monitoringServices.stream().map(service -> mapper.map(service, SlaUserDTO.class))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(serviceDtos, HttpStatus.OK);
     }
 }
